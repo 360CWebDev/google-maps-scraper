8874de470df52e1533f6ccc61d5ac06d5e900917
360CWebDev <142518681+360CWebDev@users.noreply.github.com>
2024-01-04 10:54:41 -0600

    Update main.py
    Change query

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from src import Gmaps
 
-love_it_star_it = '''Love It? Star It! ⭐ https://github.com/omkarcloud/google-maps-scraper/'''
-
 queries = [
-   "web developers in bangalore"
+   "web developers in austin texas"
 ]
 
 Gmaps.places(queries, max=5)
