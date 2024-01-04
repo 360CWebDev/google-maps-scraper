c35311cdc63cf9b46ca463242a560fee18230bfb
360CWebDev <142518681+360CWebDev@users.noreply.github.com>
2024-01-04 11:23:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 #from src import Gmaps
 #import requests
 
-import os
-url = os.environ
-print(url)
+#import os
+#url = os.environ
+#print(url)
 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/query/{queryString}")
+async def read_item(queryString):
+    return {"query": queryString}
 
 #queries = [
 #   "web developers in austin texas"
